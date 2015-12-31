1f1ae1438d3c09ee9bb76a5d2dcbd754194eb9ab
w.vanooijen92 <w.vanooijen@dualbrands.nl>
2015-12-31 15:47:04 +0100

    added process.json

--- scripts/rh-deploy.sh
+++ scripts/rh-deploy.sh
@@ -19,5 +19,5 @@ exit
 #sudo systemctl enable <%= appName %>.service
 #sudo systemctl start <%= appName %>.service
 cd <%= appRemoteTargetPath %>
-echo -e "{\"apps\":[{\"name\":\""<%= appName %>"\",\"script\":\"bundle/main.js\",\"log_date_format\":\"YYYY-MM-DD\",\"exec_mode\":\"fork_mode\",\"env\":{\"PORT\": \""<%= appPort %>"\",\"MONGO_URL\": \""<%= appMongoUrl %>"\",\"ROOT_URL\":\""<%= appRootUrl %>"\"}}]}" > process.json
+echo "{\"apps\":[{\"name\":\""<%= appName %>"\",\"script\":\"bundle/main.js\",\"log_date_format\":\"YYYY-MM-DD\",\"exec_mode\":\"fork_mode\",\"env\":{\"PORT\": \""<%= appPort %>"\",\"MONGO_URL\": \""<%= appMongoUrl %>"\",\"ROOT_URL\":\""<%= appRootUrl %>"\"}}]}" > process.json
 pm2 start process.js
