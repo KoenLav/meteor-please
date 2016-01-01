29b28e9675e85c81186807c2749d2110de7c20c9
w.vanooijen92 <w.vanooijen@dualbrands.nl>
2016-01-02 00:00:09 +0100

    nginx setup

---
+++ scripts/rh-setup-vhost.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+touch temp.sed
+echo 's/$[appSiteUrl]/<%= appSiteUrl %>' >> temp.sed
+echo 's/$[appName]/<%= appName %>' >> temp.sed
+echo 's/$[appPort]/<%= appPort %>' >> temp.sed
+
+sed -f temp.sed nginx.conf > <%= appName %>.conf
+
+rm temp.sed
+
+sudo systemctl restart nginx
