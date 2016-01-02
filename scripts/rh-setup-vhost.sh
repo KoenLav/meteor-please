138abe31ef97d90e27851304179eae060e35eb73
w.vanooijen92 <w.vanooijen@dualbrands.nl>
2016-01-02 02:41:46 +0100

    nginx setup

--- scripts/rh-setup-vhost.sh
+++ scripts/rh-setup-vhost.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd <%= appRemoteTargetPath %>
 
+perl -pi -e 's/appSiteUrl/<%= appSiteUrl %>/g' vhost.conf
+perl -pi -e 's/appName/<%= appName %>/g' vhost.conf
+perl -pi -e 's/appPort/<%= appPort %>/g' vhost.conf
+perl -pi -e 's/appDest/<%= appDest %>/g' vhost.conf
 
 sudo systemctl restart nginx
