6509fc3b3f3ffe9fb7069dac6f5974605adc514b
w.vanooijen92 <w.vanooijen@dualbrands.nl>
2016-01-02 03:49:43 +0100

    nginx setup

--- scripts/rh-setup-vhost.sh
+++ scripts/rh-setup-vhost.sh
@@ -7,5 +7,6 @@ perl -pi -e s{appPort}{<%= appPort %>}g vhost.conf
 perl -pi -e s{appDest}{<%= appDest %>}g vhost.conf
 
 sudo mv vhost.conf /etc/nginx/conf.d/<%= appName %>.conf
+sudo chown root:root /etc/nginx/conf.d/<%= appName %>.conf
 
 sudo systemctl restart nginx
