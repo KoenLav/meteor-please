3f8e533a37a8ed55a1909554b26ae361adc3e2f5
w.vanooijen92 <w.vanooijen@dualbrands.nl>
2016-01-02 03:16:01 +0100

    nginx setup

--- scripts/rh-setup-vhost.sh
+++ scripts/rh-setup-vhost.sh
@@ -5,6 +5,7 @@ perl -pi -e s{appSiteUrl}{<%= appSiteUrl %>}g vhost.conf
 perl -pi -e s{appName}{<%= appName %>}g vhost.conf
 perl -pi -e s{appPort}{<%= appPort %>}g vhost.conf
 perl -pi -e s{appDest}{<%= appDest %>}g vhost.conf
-
-sudo mv vhost.conf /etc/nginx/conf.d/sites-enabled/<%= appName %>.conf
+sudo su
+mv vhost.conf /etc/nginx/conf.d/sites-enabled/<%= appName %>.conf
+exit
 sudo systemctl restart nginx
