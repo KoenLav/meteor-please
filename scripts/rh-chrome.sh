9c420017f55231c04a19a611cb56fb883f482128
w.vanooijen92 <w.vanooijen@dualbrands.nl>
2016-01-25 01:12:56 +0100

    changes

--- scripts/rh-chrome.sh
+++ scripts/rh-chrome.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
- meteor build chrome --directory $BUILD_OPTIONS
+ meteor build ../chrome --directory $BUILD_OPTIONS
 
- cp -R $PATH_TO_CHROME ./bundle/chrome/programs/chrome
+ cp -R $PATH_TO_CHROME ../chrome/bundle/programs/chrome
