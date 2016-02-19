19ecf44e07a60f60565fa24993019cb4282d3f44
w.vanooijen92 <w.vanooijen@dualbrands.nl>
2016-02-19 17:39:55 +0100

    debug

---
+++ scripts/rh-chrome-init.sh
@@ -0,0 +1,4 @@
+ #!/bin/bash
+ meteor build ../chrome --directory $BUILD_OPTIONS
+
+ cp -R $PATH_TO_CHROME ../chrome/bundle/programs/web.chrome
--- scripts/rh-chrome.sh
+++
@@ -1,4 +0,0 @@
- #!/bin/bash
- meteor build ../chrome --directory $BUILD_OPTIONS
-
- cp -R $PATH_TO_CHROME ../chrome/bundle/programs/chrome
