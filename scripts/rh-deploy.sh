bc08ebf16ad2a6d1788a538d5c256104020afb43
w.vanooijen92 <w.vanooijen@dualbrands.nl>
2016-06-11 13:26:48 +0200

    support for meteor 1.3

--- scripts/rh-deploy.sh
+++ scripts/rh-deploy.sh
@@ -18,7 +18,7 @@ npm install
 #sudo systemctl enable <%= appName %>.service
 #sudo systemctl start <%= appName %>.service
 cd <%= appRemoteTargetPath %>
-echo "{\"apps\":[{\"name\":\"<%= appName %>\",\"script\":\"bundle/main.js\",\"log_date_format\":\"YYYY-MM-DD\",\"exec_mode\":\"fork_mode\",\"env\":{\"PORT\": \"<%= appPort %>\",\"MONGO_URL\": \"<%= appMongoUrl %>\",\"ROOT_URL\":\"<%= appRootUrl %>\", \"MAIL_URL\" : \"<%=mailUrl%>\"}}]}" > process.json
+echo "{\"apps\":[{\"name\":\"<%= appName %>\",\"exec_interpreter\":\"<%=interpreter%>\",\"script\":\"bundle/main.js\",\"log_date_format\":\"YYYY-MM-DD\",\"exec_mode\":\"fork_mode\",\"env\":{\"PORT\": \"<%= appPort %>\",\"MONGO_URL\": \"<%= appMongoUrl %>\",\"ROOT_URL\":\"<%= appRootUrl %>\", \"MAIL_URL\" : \"<%=mailUrl%>\"}}]}" > process.json
 pm2 start process.json
 
 pm2 save
